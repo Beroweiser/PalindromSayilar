760c899f63de82ad8366c8645a606dc6d3633cba
Berat Altındal <berat_altindal.97@hotmail.com>
2022-08-06 14:59:55 +0300

    add

---
+++ Palindrom.java
@@ -0,0 +1,29 @@
+/**
+ * Palindrom
+ */
+import java.util.Scanner;
+
+
+public class Palindrom {
+    static boolean isPalidrom(int number){
+        int temp = number , reverseNumber = 0, lastNumber ;
+        while(temp != 0){
+            lastNumber = temp % 10 ;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Palidrom olup olmadıgını kontrol etmek istediginiz sayıyı giriniz: ");
+        int a = inp.nextInt();
+        String palidrom = isPalidrom(a) ? "Palidrom sayıdır " : "Palidrom sayı değildir";
+        System.out.println(palidrom);
+        inp.close();
+    }
+}
